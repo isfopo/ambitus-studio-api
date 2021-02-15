ef9949134703d677f2ff5e4cbadc3b4a2a9fdb2e
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-15 12:39:30 -0500

    adds post and invite routes to project

--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
-//const User = require("../db/models").User;
+const Project = require("../db/models").Project;
 
-router.get("/", (res, req) => {
-  req.status(200).json({ message: "you made it!" });
+router.post("/", async (req, res) => {
+  try {
+    const project = await Project.create({
+      // TODO: associate user who requested to project
+      name: req.body.name,
+      tempo: req.body.tempo,
+      time_signature: req.body.time_signature,
+    });
+
+    return res.status(200).json(project);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
+});
+
+router.put("/invite", async (req, res) => {
+  try {
+    const project = findByPk(req.body.id);
+    project.invited.push(req.body.invitee); // TODO: check if invitee is in db
+    await project.save();
+
+    return res.status(200).json(project);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
 });
 
 module.exports = router;
