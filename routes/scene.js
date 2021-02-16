c41609e8e2f940496152329df94c933080986672
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-16 00:29:52 -0500

    adds post route

--- routes/scene.js
+++ routes/scene.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
-//const User = require("../db/models").User;
+const Project = require("../db/models").Project;
+const Scene = require("../db/models").Scene;
 
-router.get("/", (res, req) => {
-  req.status(200).json({ message: "you made it!" });
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "you made it!" });
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const project = await Project.findByPk(req.body.project);
+    const scene = await project.createScene();
+    return res.status(200).json(scene);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
 });
 
 module.exports = router;
