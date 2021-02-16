e62222c71e345ddd90f3b076086bdfdbe2080531
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-16 11:31:45 -0500

    sets up validation functions

---
+++ routes/handlers/helpers/validate.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const Name = (input = {}) => {};
+
+const Tempo = (input = {}) => {};
+
+const TimeSignature = (input = {}) => {};
+
+const Message = (input = {}) => {};
+
+const Setting = (input = {}) => {};
+
+const Type = (input = {}) => {};
+
+module.exports = {
+  Name,
+  Tempo,
+  TimeSignature,
+  Message,
+  Setting,
+  Type,
+};
