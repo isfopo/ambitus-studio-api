023429dfab14f2444d44b96947674e300af2200f
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-16 15:16:53 -0500

    adds tempo and message validation

--- routes/handlers/helpers/validate.js
+++ routes/handlers/helpers/validate.js
@@ -12,11 +12,33 @@ const name = (name = "") => {
   }
 };
 
-const tempo = (input) => {};
+const tempo = (tempo = 0) => {
+  if (typeof tempo !== "number") {
+    throw new Error("tempo must be a number");
+  } else if (tempo % 1 !== 0) {
+    throw new Error("tempo must be an integer");
+  } else if (tempo < 40) {
+    throw new Error("tempo must be greater than 40 bpm");
+  } else if (tempo > 280) {
+    throw new Error("tempo must be less than 280 bpm");
+  } else {
+    return tempo;
+  }
+};
 
 const timeSignature = (input) => {};
 
-const message = (input) => {};
+const message = (message = "") => {
+  if (typeof message !== "string") {
+    throw new Error("message must be a string");
+  } else if (message.length < 1) {
+    throw new Error("message must be greater than 1 character");
+  } else if (message.length > 150) {
+    throw new Error("message must be less than 150 characters");
+  } else {
+    return message;
+  }
+};
 
 const setting = (input) => {};
 
