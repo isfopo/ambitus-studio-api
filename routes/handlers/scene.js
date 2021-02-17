76c923e4870d75d5384f74d69d3d9a46c5d5a86f
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-17 14:35:41 -0500

    adds tests for validation handlers

--- routes/handlers/scene.js
+++ routes/handlers/scene.js
@@ -1,6 +1,27 @@
 const validate = require("./helpers/validate");
 
-const validatePost = (body) => {};
+const validatePost = (body) => {
+  const errors = [];
+
+  if (!body.id) {
+    errors.push("body should contain an id");
+  }
+
+  try {
+    validate.id(body.id);
+    body.name && validate.name(body.name);
+    body.tempo && validate.tempo(body.tempo);
+    body.time_signature && validate.timeSignature(body.time_signature);
+  } catch (e) {
+    errors.push(e.message);
+  }
+
+  if (errors.length > 0) {
+    throw errors;
+  } else {
+    return body;
+  }
+};
 
 module.exports = {
   validatePost,
