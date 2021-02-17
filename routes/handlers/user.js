2fa532e9b7153a16dafca4994a21e5fabcb7d992
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-17 13:34:50 -0500

    adds validate user post function

--- routes/handlers/user.js
+++ routes/handlers/user.js
@@ -1,6 +1,27 @@
 const validate = require("./helpers/validate");
 
-const validatePost = (body) => {};
+const validatePost = (body = {}) => {
+  const errors = [];
+
+  if (!body.username) {
+    errors.push("body should contain a username");
+  } else if (!body.password) {
+    errors.push("body should contain a password");
+  }
+
+  try {
+    username = validate.name(body.username);
+    password = validate.password(body.password);
+  } catch (e) {
+    errors.push(e.message);
+  }
+
+  if (errors.length > 0) {
+    throw errors;
+  } else {
+    return body;
+  }
+};
 
 module.exports = {
   validatePost,
