cbcb74c5d3b6bf97506b7008bbd1920446a23195
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-17 14:11:42 -0500

    creates handler for post validation

--- routes/handlers/project.js
+++ routes/handlers/project.js
@@ -1,6 +1,33 @@
 const validate = require("./helpers/validate");
 
-const validatePost = (body) => {};
+const validatePost = (body) => {
+  const errors = [];
+
+  if (!body.id) {
+    errors.push("body should contain a username");
+  } else if (!body.name) {
+    errors.push("body should contain a password");
+  } else if (!body.tempo) {
+    errors.push("body should contain a password");
+  } else if (!body.time_signature) {
+    errors.push("body should contain a password");
+  }
+
+  try {
+    validate.id(body.id);
+    validate.name(body.name);
+    validate.tempo(body.tempo);
+    validate.timeSignature(body.time_signature);
+  } catch (e) {
+    errors.push(e.message);
+  }
+
+  if (errors.length > 0) {
+    throw errors;
+  } else {
+    return body;
+  }
+};
 
 module.exports = {
   validatePost,
