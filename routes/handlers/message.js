5bc99b79bb7e5572d475a44b27ea55e822649a54
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-17 16:45:15 -0500

    sets up tests and handlers for messages

--- routes/handlers/message.js
+++ routes/handlers/message.js
@@ -1,6 +1,27 @@
 const validate = require("./helpers/validate");
 
-const validatePost = (body) => {};
+const validatePost = (body = {}) => {
+  const errors = [];
+
+  if (!body.id) {
+    errors.push("body should contain an id");
+  } else if (!body.content) {
+    errors.push("body should contain content");
+  }
+
+  try {
+    validate.id(body.id);
+    validate.message(body.content);
+  } catch (e) {
+    errors.push(e.message);
+  }
+
+  if (errors.length > 0) {
+    throw errors;
+  } else {
+    return body;
+  }
+};
 
 module.exports = {
   validatePost,
--- routes/tests/routes/message.test.js
+++ routes/tests/routes/message.test.js
@@ -1,2 +1,61 @@
 const assert = require("assert");
 const Message = require("../../handlers/message");
+
+describe("Message.validatePost", () => {
+  describe("when body contains id and content", () => {
+    it("should return body", () => {
+      const input = {
+        id: "709ae450-7017-11eb-beda-e76c7ddac317",
+        content: "This sounds great!!!",
+      };
+      const output = Message.validatePost(input);
+      assert.deepStrictEqual(output, input);
+    });
+  });
+
+  describe("when body does not contain an id", () => {
+    it("should throw an error", () => {
+      const input = {
+        content: "This sounds great!!!",
+      };
+      assert.throws(() => {
+        const output = Message.validatePost(input);
+      });
+    });
+  });
+
+  describe("when body does not contain content", () => {
+    it("should throw an error", () => {
+      const input = {
+        id: "709ae450-7017-11eb-beda-e76c7ddac317",
+      };
+      assert.throws(() => {
+        const output = Message.validatePost(input);
+      });
+    });
+  });
+
+  describe("when id is not a valid uuid", () => {
+    it("should throw an error", () => {
+      const input = {
+        id: "1234567890abcdef",
+        content: "This sounds great!!!",
+      };
+      assert.throws(() => {
+        const output = Message.validatePost(input);
+      });
+    });
+  });
+
+  describe("when content is not a valid message", () => {
+    it("should throw an error", () => {
+      const input = {
+        id: "709ae450-7017-11eb-beda-e76c7ddac317",
+        content: 12345,
+      };
+      assert.throws(() => {
+        const output = Message.validatePost(input);
+      });
+    });
+  });
+});
