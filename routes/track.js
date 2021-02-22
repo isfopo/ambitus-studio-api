c4ed446d0b94be22a096ff062d70cc1047480015
Isaac Poole <isaacpoolemusic@gmail.com>
2021-02-21 23:56:17 -0500

    adds post and get track route

--- routes/track.js
+++ routes/track.js
@@ -1,9 +1,55 @@
 const express = require("express");
 const router = express.Router();
-//const User = require("../db/models").User;
 
-router.get("/", (res, req) => {
-  req.status(200).json({ message: "you made it!" });
+const Project = require("./handlers/project");
+const Track = require("./handlers/track");
+
+/**
+ * Create a new track in a project (Authorization Bearer Required)
+ * @route POST /track
+ * @group track - Operations about track
+ * @param {string} id.body.required - containing project's id
+ * @param {integer} name.body.required - new track's name
+ * @param {string} settings.body.required - new track's settings
+ * @param {string} type.body.required - new track's type
+ * @returns {object} 200 - An object of track's info with generated track id
+ * @returns {Error}  400 - Invalid token, name, tempo or time signature
+ */
+router.post("/", Project.authorize, async (req, res) => {
+  try {
+    const trackParameters = Track.validatePost(req.body);
+
+    const track = await req.project.createTrack({
+      name: trackParameters.name,
+      settings: trackParameters.settings,
+      type: trackParameters.type,
+    });
+
+    return res.status(200).json(track);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+});
+
+/**
+ * Get track information (Authorization Bearer Required)
+ * @route GET /track
+ * @group track - Operations about track
+ * @param {string} projectId.body.required - project's id
+ * @param {string} trackId.body.required - track's id
+ * @returns {object} 200 - An object of track's info with generated track id
+ * @returns {Error}  400 - Invalid token or id
+ * @returns {Error}  403 - User is not in project
+ * @returns {Error}  404 - Track not found
+ */
+router.get("/", Project.authorize, async (req, res) => {
+  try {
+    const track = Track.isInDatabase(req.body.trackId);
+    return res.status(200).json(track);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 });
 
 module.exports = router;
