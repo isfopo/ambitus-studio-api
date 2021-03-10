811a040ed6c79a21c592b3ab63ce58eb757e15b8
Isaac Poole <isaacpoolemusic@gmail.com>
2021-03-10 12:52:03 -0500

    removes post, adds get

--- routes/clip.js
+++ routes/clip.js
@@ -5,39 +5,94 @@ const multer = require("multer");
 const upload = multer({ dest: __dirname + "/temp/" });
 const fs = require("fs");
 
+const models = require("../db/models");
 const Project = require("./handlers/project");
 const Scene = require("./handlers/scene");
 const Track = require("./handlers/track");
 const Clip = require("./handlers/clip");
 
+// IDEA: maybe have a way to get clip by track and scene
 /**
- * Create a new clip in a project (Authorization Bearer Required)
- * @route POST /clip
- * @group clip - Operations about clip
- * @param {string} projectId.body.required - containing project's id
- * @param {string} sceneId.body.required - containing project's id
- * @param {string} trackId.body.required - containing project's id
- * @param {string} name.body.optional - new clip's name
- * @param {integer} tempo.body.required - new clip's tempo
- * @param {string} time_signature.body.required - new clip's time signature
- * @param {integer} type.body.required - new clip's type
- * @returns {object} 200 - An object of clip's info with generated clip id
- * @returns {Error}  400 - Invalid token, name, tempo or time signature
+ * Get all clips in project, scene or track (Authorization Bearer Required)
+ * @route GET /clip
+ * @group clip - Operations about clips
+ * @param {String} ProjectId.body.required
+ * @param {String} SceneId.body.optional
+ * @param {String} TrackId.body.optional
+ * @returns {Array} 200 - A nested array of clips in project,  organized by scene then track
  */
-router.post("/", Project.authorize, Clip.post);
+router.get("/", Project.authorize, async (req, res) => {
+  const scenes = await req.project.getScenes(
+    req.body.SceneId
+      ? {
+          where: { SceneId: req.body.SceneId },
+        }
+      : null
+  );
 
-// TODO: maybe have a way to get clip by track and scene
-router.get("/", Project.authorize, Clip.get);
+  const tracks = await req.project.getTracks(
+    req.body.TrackId
+      ? {
+          where: { TrackId: req.body.TrackId },
+        }
+      : null
+  );
 
-router.get("/content", Project.authorize, Clip.getContent);
+  const clips = [];
 
-router.put("/name", Project.authorize, upload.single("content"), Clip.putName);
+  for (let i = 0; i < scenes.length; i++) {
+    const sceneClips = [];
+    for (let j = 0; j < tracks.length; j++) {
+      const trackClips = await tracks[j].getClips({
+        where: { SceneId: scenes[i].SceneId },
+      });
+      sceneClips.push({ [tracks[j].TrackId]: trackClips[0] });
+    }
+    clips.push({ [scenes[i].SceneId]: sceneClips });
+  }
 
+  return res.status(200).json(clips);
+});
+
+router.get("/content", Project.authorize, async (req, res) => {
+  const clip = await findInDatabase(req.body.ClipId);
+  if (clip.content) {
+    const stream = Readable.from(clip.content);
+    stream.pipe(res);
+  } else {
+    return res.status(400).json({ error: e.message });
+  }
+});
+
+router.put("/name", Project.authorize, async (req, res) => {
+  const clip = await User.findByPk(req.body.id);
+
+  clip.name = req.body.name;
+  await clip.save();
+
+  res.status(200).json(clip);
+});
+
+// new clip must match tracks, mimetype and have tempo and time signature
 router.put(
   "/content",
   Project.authorize,
   upload.single("content"),
-  Clip.putContent
+  async (req, res) => {
+    const clip = await findInDatabase(req.body.id);
+
+    if ((clip.type = req.file.mimetype)) {
+      const clipBuffer = fs.readFileSync(req.file.path);
+      clip.content = clip;
+      await clip.save();
+
+      res.status(200).json(clip);
+    } else {
+      return res.status(400).json({
+        error: "Clip type cannot be changed. Put clip in new track instead.",
+      });
+    }
+  }
 );
-// TODO: new clip must match tracks, mimetype and have tempo and time signature
+
 module.exports = router;
