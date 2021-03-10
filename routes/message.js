b506bb4828e4e9eaaf2fb7adeb9686b9772529c1
Isaac Poole <isaacpoolemusic@gmail.com>
2021-03-10 16:09:38 -0500

    adds get and post route to message

--- routes/message.js
+++ routes/message.js
@@ -3,16 +3,33 @@ const router = express.Router();
 const Project = require("./handlers/project");
 const Message = require("./handlers/message");
 
+/**
+ * Create a new message (Authorization Bearer Required)
+ * @route POST /message
+ * @group message - Operations about messages
+ * @param {String} ProjectId.body.required - project's id
+ * @param {String} content.body.required - message content to post
+ * @returns {object} 204
+ */
 router.post("/", Project.authorize, async (req, res) => {
-  const message = await req.project.createMessage(
-    Message.validatePost(req.body)
-  );
-  await req.user.addMessage(message);
-  return res.status(204).json(message);
+  try {
+    await req.user.addMessage(
+      await req.project.createMessage(Message.validatePost(req.body))
+    );
+    return res.sendStatus(204);
+  } catch (e) {
+    return res.status(400).json({ error: e.message });
+  }
 });
 
-router.get("/", async (req, res) => {
-  return res.status(200).json({ msg: "you made it!" });
+/**
+ * Gets all messages from project (Authorization Bearer Required)
+ * @route GET /project
+ * @group message - Operations about messages
+ * @returns {Array} 200 - an array of messages in project
+ */
+router.get("/", Project.authorize, async (req, res) => {
+  return res.status(200).json(await req.project.getMessages());
 });
 
 module.exports = router;
