2a467fa59352daa3a5c25749273112d84e626650
Isaac Poole <isaacpoolemusic@gmail.com>
2021-03-25 17:59:11 -0400

    does not require ProjectId in body

--- routes/handlers/message.js
+++ routes/handlers/message.js
@@ -3,14 +3,11 @@ const validate = require("./helpers/validate");
 const validatePost = (body = {}) => {
   const errors = [];
 
-  if (!body.ProjectId) {
-    errors.push("body should contain a ProjectId");
-  } else if (!body.content) {
+  if (!body.content) {
     errors.push("body should contain content");
   }
 
   try {
-    validate.id(body.ProjectId);
     validate.message(body.content);
   } catch (e) {
     errors.push(e.message);
